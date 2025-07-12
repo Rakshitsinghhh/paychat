b6adc81cb023303a29bf018da29e137447c4dc5c
Rakshit Singh <singhrakshit2404@gmail.com>
2025-07-12 21:42:50 +0000

    register backend

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,13 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+const ws = new WebSocket("ws://localhost:8080")
+
 export async function POST(req: NextRequest) {
   const data = await req.json();
   const { name } = data;
 
-
+  const payload = JSON.stringify({
+    type: "register",
+    userId : name
+  })
+  const resp = ws.send(payload)  
 
   return NextResponse.json({
-    username: name
+    username: name,
+    responsefromws: resp
   });
 }
